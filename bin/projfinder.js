0a087480fe82413008914c4ce2dc927108801f14
Aaron Racicot <aaronr@z-pulley.com>
2012-12-27 18:53:43 +0000

    getting the reprojection tool switched to using the database in node as well as returning valid geojson

--- bin/projfinder.js
+++ bin/projfinder.js
@@ -1,9 +1,5 @@
 /*
-**
-**  requires 
-**  npm install express
-**  npm install socket.io
-**
+  ProjFinder
 */
 var app = require('express')();
 var express = require("express");
@@ -13,98 +9,98 @@ server.listen(9000);
 var pg = require('pg');
 
 
-/*
-**
-**  setup static and index
-**
-*/
-//app.get('/', function (req, res) {
-//
-//	res.sendfile(__dirname + '/index.html');
-//
-//});
-//app.use("/static", express.static(__dirname + '/static'));
-/*app.use("/public", express.static(__dirname + '/public'));*/
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/index.html');
+});
+app.use("/static", express.static(__dirname + '/static'));
 
-/*
-**
-**  app without socketid
-**
-*/
-
-/*
-**
-**  URL SCHEME
-**
-
-	app.get('/reproject', function (req, res) :
-	
-		this API endpoint with do reprojection	
-
-	app.get('/guess', function (req, res) :
-		
-		this API 
-**
-**
-*/
 app.get('/reproject', function (req, res) {
-
-	var x = req.query['x'];
-	var y = req.query['y'];
-	var epsg = req.query['epsg'];
-	res.jsonp( { 'epsg' : epsg, 'x' : x, 'y' : y } );
-
+    function ReprojectionObject(){
+	this.type = 'Reprojection';
+	this.from = new Array();
+	this.to = new Array();
+    };
+    
+    var x = req.query['x'];
+    var y = req.query['y'];
+    var epsg = req.query['epsg'];
+    if (!!x && !!y && !!epsg) {
+        //res.jsonp({'epsg':epsg,'x':x,'y':y});
+        var connString = 'postgres://aaronr:aaronr@localhost:5432/projfinder';
+        pg.connect(connString, function(err, client) {   
+            if (err) {
+                res.jsonp({'error':err});
+                console.log(err);
+            } else {
+		var sql = "select st_asgeojson(st_geometryfromtext('POINT(" + x + " " + y + ")',4326),15,4) as from, st_asgeojson(st_transform(st_geometryfromtext('POINT(" + x + " " + y + ")',4326)," + epsg + "),15,4) as to;"
+		console.log(sql);
+		//var sql = "select alias_code from epsg_alias";
+                client.query(sql, function(err,result) {
+                    if ( err ) {
+                        res.jsonp({'error':err});
+                        console.log(err);                       
+                    } else {
+                        var reprojection = new ReprojectionObject();
+                        for (i = 0; i < result.rows.length; i++) {
+                            reprojection.from[i] = JSON.parse(result.rows[i].from);
+                            reprojection.to[i] = JSON.parse(result.rows[i].to);
+                        }
+                        res.jsonp( reprojection );
+                    }
+                });
+            }
+        });
+    } else {
+        res.jsonp({'error':'invalid query parameters'});
+    }
 });
 
-/*
-**
-** EXAMPLE
-**
-*/
 app.get('/psql', function (req, res) {
-
-	function FeatureCollection(){
-	    this.type = 'FeatureCollection';
-	    this.features = new Array();
-	}
-
-	var connString = 'postgres://aaronr:aaronr@localhost:5432/projfinder';
-
-	pg.connect(connString, function(err, client) {
-	 
-		if ( err ) {
-
-		   console.log( err );			
-
-		}
-
-		var sql = "select alias_code from epsg_alias";
-	 
-		client.query(sql, function(err, result) {
-	 
-		    if ( err ) {
-
-			console.log( err );			
-
-		    }
-
-		    var featureCollection = new FeatureCollection();
-
-		    for (i = 0; i < result.rows.length; i++)
-		    {
-			featureCollection.features[i] = JSON.parse( result.rows[i].alias_code );
-		    }
-
-		    //res.writeHead(200, { 'Content-Type': 'application/json' });
-		    //res.write( JSON.stringify( { 'hello' : 'goodbye' } ) );
-		    res.jsonp( featureCollection );
-	 
-		});
-
-
-	}); // END pg.connect
-
+    //function FeatureCollection(){
+    //    this.type = 'FeatureCollection';
+    //    this.features = new Array();
+    //}
+    var connString = 'postgres://gcorradini:gcorradini@localhost:5432/projfinder';
+    pg.connect(connString, function(err, client) {
+        if ( err ) {
+            console.log( err );                  
+        }
+        var sql = "select alias_code from epsg_alias";
+        client.query(sql, function(err, result) {
+            if ( err ) {
+                console.log( err );                     
+            }
+            var featureCollection = new FeatureCollection();
+            for (i = 0; i < result.rows.length; i++)
+            {
+                featureCollection.features[i] = JSON.parse( result.rows[i].alias_code );
+            }
+            //res.writeHead(200, { 'Content-Type': 'application/json' });
+            //res.write( JSON.stringify( { 'hello' : 'goodbye' } ) );
+            res.jsonp( featureCollection );
+        });
+    }); // END pg.connect
 }); // END psql
+
+/*app.use("/public", express.static(__dirname + '/public'));*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /*
@@ -113,171 +109,171 @@ app.get('/psql', function (req, res) {
 **
 */
 /*
-io.sockets.on('connection', function (socket) {
+  io.sockets.on('connection', function (socket) {
 
 
-	function FeatureCollection(){
-	    this.type = 'FeatureCollection';
-	    this.features = new Array();
-	}
+  function FeatureCollection(){
+  this.type = 'FeatureCollection';
+  this.features = new Array();
+  }
 
- 
-	pg.connect(connString, function(err, client) {
-	 
-		if ( err ) {
+  
+  pg.connect(connString, function(err, client) {
+  
+  if ( err ) {
 
-		   console.log( err );			
+  console.log( err );                  
 
-		}
+  }
 
-		var sql = 'select ST_X(geom_wgs84) as lon, ST_Y(geom_wgs84) as lat from common_interest;';
-	 
-		client.query(sql, function(err, result) {
-	 
-		    var coordinates = new Array();
+  var sql = 'select ST_X(geom_wgs84) as lon, ST_Y(geom_wgs84) as lat from common_interest;';
+  
+  client.query(sql, function(err, result) {
+  
+  var coordinates = new Array();
 
-		    for (i = 0; i < result.rows.length; i++)
-		    {
-			coordinates[i] = { lat: result.rows[i].lat, lon: result.rows[i].lon };
-		    }
+  for (i = 0; i < result.rows.length; i++)
+  {
+  coordinates[i] = { lat: result.rows[i].lat, lon: result.rows[i].lon };
+  }
 
-		    socket.emit( 'coordinates', coordinates );
-	 
-		});
-
-
-        }); // END pg.connect
-
-	socket.on( 'load_coordinates', function( data ) {
-
-		if (! data.bbox ) {
-			socket.emit( 'coordinates', { 'success' : false } );	
-		}
-		pg.connect(connString, function(err, client) {
-		 
-			if ( err ) {
-
-			   console.log( err );			
-
-			}
-
-			var sql = "select ST_X(geom_wgs84) as lon, ST_Y(geom_wgs84) as lat " + 
-			"FROM common_interest " +
-			"WHERE ST_Within( geom_wgs84, ST_GeomFromText( 'POLYGON((" +
-			data.bbox[0] +" "+ data.bbox[1] +","+ data.bbox[0] +" "+ data.bbox[3] +","+ data.bbox[2] +" "+ data.bbox[3] +
-			","+ data.bbox[2] +" "+ data.bbox[1] +","+ data.bbox[0] +" "+ data.bbox[1] +"))', 4326 ) )";
-		 
-			client.query(sql, function(err, result) {
-		 
-			    if ( err ) {
-
-				console.log( err );			
-
-			    }
-			    var coordinates = new Array();
-
-			    for (i = 0; i < result.rows.length; i++)
-			    {
-				coordinates[i] = { lat: result.rows[i].lat, lon: result.rows[i].lon };
-			    }
-
-			    console.log( 'coordinates length = ' + coordinates.length );
-			    socket.emit( 'bbox_coordinates', coordinates );
-		 
-			});
+  socket.emit( 'coordinates', coordinates );
+  
+  });
 
 
-		}); // END pg.connect
+  }); // END pg.connect
 
-	}); // END socket.on
+  socket.on( 'load_coordinates', function( data ) {
 
-	socket.on( 'load_polygons', function( data ) {
+  if (! data.bbox ) {
+  socket.emit( 'coordinates', { 'success' : false } );    
+  }
+  pg.connect(connString, function(err, client) {
+  
+  if ( err ) {
 
-		if (! data.bbox ) {
-			socket.emit( 'bbox_polygons', { 'success' : false } );	
-		}
-		pg.connect(connString, function(err, client) {
-		 
-			if ( err ) {
+  console.log( err );                  
 
-			   console.log( err );			
+  }
 
-			}
+  var sql = "select ST_X(geom_wgs84) as lon, ST_Y(geom_wgs84) as lat " + 
+  "FROM common_interest " +
+  "WHERE ST_Within( geom_wgs84, ST_GeomFromText( 'POLYGON((" +
+  data.bbox[0] +" "+ data.bbox[1] +","+ data.bbox[0] +" "+ data.bbox[3] +","+ data.bbox[2] +" "+ data.bbox[3] +
+  ","+ data.bbox[2] +" "+ data.bbox[1] +","+ data.bbox[0] +" "+ data.bbox[1] +"))', 4326 ) )";
+  
+  client.query(sql, function(err, result) {
+  
+  if ( err ) {
 
-			var sql = "select ST_AsGeoJSON( geom_wgs84 )  as shape " + 
-			"FROM dstcode " +
-			"WHERE ST_Intersects( geom_wgs84, ST_GeomFromText( 'POLYGON((" +
-			data.bbox[0] +" "+ data.bbox[1] +","+ data.bbox[0] +" "+ data.bbox[3] +","+ data.bbox[2] +" "+ data.bbox[3] +
-			","+ data.bbox[2] +" "+ data.bbox[1] +","+ data.bbox[0] +" "+ data.bbox[1] +"))', 4326 ) )";
-		 
-			client.query(sql, function(err, result) {
-		 
-			    if ( err ) {
+  console.log( err );                     
 
-				console.log( err );			
+  }
+  var coordinates = new Array();
 
-			    }
-		 	    var featureCollection = new FeatureCollection();
+  for (i = 0; i < result.rows.length; i++)
+  {
+  coordinates[i] = { lat: result.rows[i].lat, lon: result.rows[i].lon };
+  }
 
-			    for (i = 0; i < result.rows.length; i++)
-			    {
-				//console.log( result.rows[i] );
-				//polygons[i] = {  obj : result.rows[i] };
-				featureCollection.features[i] = JSON.parse(result.rows[i].shape);
-			    }
-
-			    socket.emit( 'bbox_polygons', featureCollection );
-		 
-			});
+  console.log( 'coordinates length = ' + coordinates.length );
+  socket.emit( 'bbox_coordinates', coordinates );
+  
+  });
 
 
-		}); // END pg.connect
+  }); // END pg.connect
 
-	}); // END socket.on
+  }); // END socket.on
+
+  socket.on( 'load_polygons', function( data ) {
+
+  if (! data.bbox ) {
+  socket.emit( 'bbox_polygons', { 'success' : false } );  
+  }
+  pg.connect(connString, function(err, client) {
+  
+  if ( err ) {
+
+  console.log( err );                  
+
+  }
+
+  var sql = "select ST_AsGeoJSON( geom_wgs84 )  as shape " + 
+  "FROM dstcode " +
+  "WHERE ST_Intersects( geom_wgs84, ST_GeomFromText( 'POLYGON((" +
+  data.bbox[0] +" "+ data.bbox[1] +","+ data.bbox[0] +" "+ data.bbox[3] +","+ data.bbox[2] +" "+ data.bbox[3] +
+  ","+ data.bbox[2] +" "+ data.bbox[1] +","+ data.bbox[0] +" "+ data.bbox[1] +"))', 4326 ) )";
+  
+  client.query(sql, function(err, result) {
+  
+  if ( err ) {
+
+  console.log( err );                     
+
+  }
+  var featureCollection = new FeatureCollection();
+
+  for (i = 0; i < result.rows.length; i++)
+  {
+  //console.log( result.rows[i] );
+  //polygons[i] = {  obj : result.rows[i] };
+  featureCollection.features[i] = JSON.parse(result.rows[i].shape);
+  }
+
+  socket.emit( 'bbox_polygons', featureCollection );
+  
+  });
 
 
-	socket.on( 'load_geojson_points', function( data ) {
+  }); // END pg.connect
 
-		if (! data.bbox ) {
-			socket.emit( 'bbox_geojson_points', { 'success' : false } );	
-		}
-		pg.connect(connString, function(err, client) {
-		 
-			if ( err ) {
-
-			   console.log( err );			
-
-			}
-
-			var sql = "select ST_AsGeoJSON( geom_wgs84 )  as shape " + 
-			"FROM common_interest " +
-			"WHERE ST_Within( geom_wgs84, ST_GeomFromText( 'POLYGON((" +
-			data.bbox[0] +" "+ data.bbox[1] +","+ data.bbox[0] +" "+ data.bbox[3] +","+ data.bbox[2] +" "+ data.bbox[3] +
-			","+ data.bbox[2] +" "+ data.bbox[1] +","+ data.bbox[0] +" "+ data.bbox[1] +"))', 4326 ) )";
-		 
-			client.query(sql, function(err, result) {
-		 
-			    if ( err ) {
-
-				console.log( err );			
-
-			    }
-
-			    var featureCollection = new FeatureCollection();
-
-			    for (i = 0; i < result.rows.length; i++)
-			    {
-				featureCollection.features[i] = JSON.parse(result.rows[i].shape);
-			    }
-
-			    socket.emit( 'bbox_geojson_points', featureCollection );
-		 
-			});
+  }); // END socket.on
 
 
-		}); // END pg.connect
+  socket.on( 'load_geojson_points', function( data ) {
 
-	}); // END socket.on
-});
+  if (! data.bbox ) {
+  socket.emit( 'bbox_geojson_points', { 'success' : false } );    
+  }
+  pg.connect(connString, function(err, client) {
+  
+  if ( err ) {
+
+  console.log( err );                  
+
+  }
+
+  var sql = "select ST_AsGeoJSON( geom_wgs84 )  as shape " + 
+  "FROM common_interest " +
+  "WHERE ST_Within( geom_wgs84, ST_GeomFromText( 'POLYGON((" +
+  data.bbox[0] +" "+ data.bbox[1] +","+ data.bbox[0] +" "+ data.bbox[3] +","+ data.bbox[2] +" "+ data.bbox[3] +
+  ","+ data.bbox[2] +" "+ data.bbox[1] +","+ data.bbox[0] +" "+ data.bbox[1] +"))', 4326 ) )";
+  
+  client.query(sql, function(err, result) {
+  
+  if ( err ) {
+
+  console.log( err );                     
+
+  }
+
+  var featureCollection = new FeatureCollection();
+
+  for (i = 0; i < result.rows.length; i++)
+  {
+  featureCollection.features[i] = JSON.parse(result.rows[i].shape);
+  }
+
+  socket.emit( 'bbox_geojson_points', featureCollection );
+  
+  });
+
+
+  }); // END pg.connect
+
+  }); // END socket.on
+  });
 */
 
